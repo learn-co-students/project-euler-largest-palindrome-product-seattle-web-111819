2cf758c5a43f8ab9ab17cb5fc14a63efba298f2d
bahayg <bahayg@gmail.com>
2020-12-05 11:38:59 -0800

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,13 @@
 # Implement your procedural solution here!
+def largest_palindrome_product
+    max = 0
+    (100..999).each do |num1|
+        (100..999).each do |num2|
+            product = num1*num2
+            if (product.to_s == product.to_s.reverse) && product > max
+                max = product
+            end
+        end
+    end
+    max
+end
